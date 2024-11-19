80648daa3f7ffc8009b637a2ed0d682af643afeb
LPT-08 <LPT-08@192.168.1.62>
2024-11-19 14:23:19 +0530

    commit

---
+++ SVKP_college/src/basicsofjava/RelationalOperator.java
@@ -0,0 +1,16 @@
+package basicsofjava;
+
+public class RelationalOperator {
+
+	public static void main(String[] args) {
+        int a=10 ,b=5;
+		
+		System.out.println("a==b "+ (a==b)); //equal to 
+		System.out.println("a!=b " + (a!=b)); //Not equal to 
+		System.out.println("a>b " + (a>b)); //Greater than 
+		System.out.println("a<b " + (a<b)); //less than 
+		System.out.println("a>=b " + (a>=b));//greater than or equal to 
+		System.out.println("a<=b " + (a<=b));//less than or equal to 
+	}
+
+}
