ad36fe195eadbbf203d0b02ae9b540bb4004f5e0
LPT-08 <LPT-08@192.168.1.62>
2024-11-19 14:54:43 +0530

    commit

---
+++ SVKP_college/src/org/tnsif/capgemini/c2tc/Jr_Developer/KIA_Speed_Limit.java
@@ -0,0 +1,17 @@
+package org.tnsif.capgemini.c2tc.Jr_Developer;
+
+public class KIA_Speed_Limit {
+	 public void disp_Speed_limit()
+		{
+			System.out.println("All KIA have speedlimit ");
+		}
+
+	public static void main(String[] args) 
+		{
+		 KIA_Speed_Limit obj=new KIA_Speed_Limit();
+		 obj.disp_Speed_limit();
+	 }
+		
+	}
+
+
