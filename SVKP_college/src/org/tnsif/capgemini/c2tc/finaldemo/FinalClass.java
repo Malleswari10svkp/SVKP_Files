e43b5c2deeb0804191de74a6afa84ac5fba05acc
LPT-08 <LPT-08@192.168.1.66>
2024-12-17 15:05:06 +0530

    commit

---
+++ SVKP_college/src/org/tnsif/capgemini/c2tc/finaldemo/FinalClass.java
@@ -0,0 +1,29 @@
+
+package org.tnsif.capgemini.c2tc.finaldemo;
+
+final class calc //hema
+{
+	public void show()
+	{
+		System.out.println("AI integration done by hema");
+	}
+}
+
+class adavcalc extends  cal //yasar
+{
+	public void finalshow()
+	{
+		System.out.println("AI integration done by yasar");
+	}
+}
+
+
+
+public class Finalclass {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
